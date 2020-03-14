bb1df49fec557809d5c1dea0c94c100f139bb704
Tehila Chasidim <tehilahchasidim@gmail.com>
2020-03-14 22:25:47 +0200

    finish styling contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="contact">
         <div class="contact-form">
+            <h1>דברו איתנו</h1>
             <form action="" method="get">
                 <input type="text" name="name" placeholder="שם פרטי" required>
                 <input type="text" name="last-name" placeholder="משפחה">
                 <input type="text" name="phone" placeholder="טלפון" required>
                 <input type="email" name="email" placeholder="אימייל">
                 <textarea id="w3mission" rows="4" cols="50" placeholder="הודעה"></textarea>
-                <input type="submit" value="שלח">
+                <input type="submit" value="שליחה">
             </form>
         </div>
         <Footer></Footer>
@@ -27,9 +28,19 @@ export default {
 
 <style scoped>
 
+    .contact{
+        background-color: rgba(0, 86, 97, 0.2);
+    }
+
     .contact-form{
         width: fit-content;
         margin: auto;
+        padding: 5rem 0;
+    }
+
+    h1{
+        font-size: 3rem;
+        margin-bottom: 3rem;
     }
 
     input, textarea{
@@ -42,19 +53,41 @@ export default {
     }
 
     input[type="submit"]{
-        background-color: rgb(230, 24, 24);
+        background-color: rgb(219, 134, 236);
         color: white;
         border: none;
-        width: 7rem;
+        font-size: 1.5rem;
         text-align: center;
     }
 
    input[type="submit"]:hover{
-        background-color: white;
-        color: rgb(230, 24, 24);
         font-weight: bold;
         cursor: pointer;
+        color: #2c3e50;;
     }
+
+    textarea{
+        resize: none;
+        font-family: 'Alef', sans-serif;
+    }
+
+@media (min-width: 51rem)
+{
+    form{
+        max-width: 35rem;
+        
+    }
+    input, textarea{
+        display: inline-block;
+        margin: 1rem 0.5rem;
+        width: 15rem;
+    }
+
+    textarea, input[type="submit"]{
+        width: 31rem;
+    } 
+    
+}
   
 </style>
 
